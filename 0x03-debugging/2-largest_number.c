2c6e14394bc7939c24b504e4ed1bca711899eb0c
Gidbolu <bgideon0439@gmail.com>
2023-06-05 16:10:55 +0100

    0. Multiple mains

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,19 +5,22 @@
  * main - prints the largest of 3 integers
  * Return: 0
  */
-
-int main(void)
+int largest_number(int a, int b, int c)
 {
-	int a, b, c;
 	int largest;
 
-	a = 972;
-	b = -98;
-	c = 0;
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-	largest = largest_number(a, b, c);
-
-	printf("%d is the largest number\n", largest);
-
-	return (0);
+	return (largest);
 }
